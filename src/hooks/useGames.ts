d7256ec17f497e865ba5b0a555f23a8792e6c8d6
Samaneh Mousavi <samaneh@Samanehs-MBP.attlocal.net>
2023-09-12 13:11:35 -0500

    Refactoring: Fetch games from react-query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
-import { Genre } from "./useGenres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from '../services/api-client';
 
 
 
@@ -20,13 +21,22 @@ export interface Game {
 }
 
 const useGames = (
-  gameQuery: GameQuery) => useData<Genre>('/games', {
-    params:
-     {genres: gameQuery.genre?.id,
-       platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      }},
-     [gameQuery]);
+  gameQuery: GameQuery) => {return useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () => 
+      apiClient
+        .get<FetchResponse<Game>>('/games', {
+          params:
+            {
+              genres: gameQuery.genre?.id,
+              parent_platforms: gameQuery.platform?.id,
+              ordering: gameQuery.sortOrder,
+              search: gameQuery.searchText
+              }
+        })
+        .then(res => res.data),
+  });
+}
+  
 
 export default useGames;

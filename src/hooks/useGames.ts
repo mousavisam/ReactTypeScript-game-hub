947ab16a155e74676e5ae149d36e25051273118a
Samaneh Mousavi <samaneh@Samanehs-MBP.attlocal.net>
2023-09-12 16:58:11 -0500

    Implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient from "../services/api-client";
 import { FetchResponse } from '../services/api-client';
@@ -16,9 +16,9 @@ export interface Game {
 }
 
 const useGames = (
-  gameQuery: GameQuery) => {return useQuery<FetchResponse<Game>, Error>({
+  gameQuery: GameQuery) => {return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () => 
+    queryFn: ({pageParam = 1}) => 
       apiClient
         .getAll({
           params:
@@ -26,9 +26,13 @@ const useGames = (
               genres: gameQuery.genre?.id,
               parent_platforms: gameQuery.platform?.id,
               ordering: gameQuery.sortOrder,
-              search: gameQuery.searchText
+              search: gameQuery.searchText,
+              page: pageParam
               }
         }),
+    getNextPageParam: (lastPage, allPages)  => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    }  
   });
 }
   

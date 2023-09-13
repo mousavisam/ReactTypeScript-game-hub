e3f78e9ecfe3b398ef847920ba0ddd01b0375057
Samaneh Mousavi <samaneh@Samanehs-MBP.attlocal.net>
2023-09-13 11:02:55 -0500

    Refactor: Simplify game query

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,13 @@ import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
-import { Genre } from "./hooks/useGenres";
 import PlatformSelecter from "./components/PlatformSelector";
-import { Platform } from "./hooks/usePlatforms";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -42,8 +40,10 @@ function App() {
         <Show above="lg">
           <GridItem area={"aside"} paddingX={2}>
             <GenreList
-              selectedGenre={gameQuery.genre}
-              onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+              selectedGenreId={gameQuery.genreId}
+              onSelectGenre={(genre) =>
+                setGameQuery({ ...gameQuery, genreId: genre.id })
+              }
             />
           </GridItem>
         </Show>
@@ -54,9 +54,9 @@ function App() {
             <Flex marginBottom={5}>
               <Box marginRight={5}>
                 <PlatformSelecter
-                  selectedPlatform={gameQuery.platform}
+                  selectedPlatformId={gameQuery.platformId}
                   onSelectPlatform={(platform) =>
-                    setGameQuery({ ...gameQuery, platform })
+                    setGameQuery({ ...gameQuery, platformId: platform.id })
                   }
                 />
               </Box>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,8 +23,8 @@ const useGames = (
         .getAll({
           params:
             {
-              genres: gameQuery.genre?.id,
-              parent_platforms: gameQuery.platform?.id,
+              genres: gameQuery.genreId,
+              parent_platforms: gameQuery.platformId,
               ordering: gameQuery.sortOrder,
               search: gameQuery.searchText,
               page: pageParam

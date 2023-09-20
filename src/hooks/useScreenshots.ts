fbce096a4056a44a12751416aee9e76b7fd3ed3e
Samaneh Mousavi <samaneh@Samanehs-MBP.attlocal.net>
2023-09-20 11:53:55 -0500

    Fix the nav bar

--- src/hooks/useScreenshots.ts
+++ src/hooks/useScreenshots.ts
@@ -2,11 +2,11 @@ import { useQuery } from "@tanstack/react-query"
 import APIClient from "../services/api-client"
 import { Screenshot } from "../entities/Screenshot";
 
-const useScreenshots = (screenshotId: number) => {
-  const apiClient = new APIClient<Screenshot>(`/games/${screenshotId}/screenshots`);
+const useScreenshots = (gameId: number) => {
+  const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
 
   return useQuery({
-    queryKey: ['screenshots', screenshotId],
+    queryKey: ['screenshots', gameId],
     queryFn: apiClient.getAll,
   })
 }
